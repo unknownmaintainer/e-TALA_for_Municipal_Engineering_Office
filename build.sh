2614372f19063f131fa576319248c1b1daebac6d
mardionfuerte <mardionjrcordetafuerte@gmail.com>
2026-08-11 01:17:36 +0800

    build: add automatic migrate and seed steps to build.sh for Render deployment

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.py migrate
+python manage.py seed_requirement_templates
+python manage.py seed_users
